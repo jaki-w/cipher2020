0a60e21d56b712b62b28465d7145672d8dee6daf
Jaki Widby <jaki.widby@gmail.com>
2020-08-10 14:13:41 -0700

    create functions to seperate and reverse user input sentence.
    Co-Authored by: Ophelia Swen <ophelia.swen@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,43 @@
 const sentence = prompt ("Enter a sentence:");
 
+let twoLetters = [];
+
 function firstLetterSeperator() {
   const firstLetter = sentence.charAt(0).toUpperCase();
-  alert(firstLetter);
+  const lastLetter = sentence.charAt(sentence.length -1).toUpperCase();
+  twoLetters.push(firstLetter);
+  twoLetters.push(lastLetter);
+  console.log(twoLetters);
 }
 
-firstLetterSeperator();
+function reverseLetters() {
+  const reversedLetters = twoLetters.reverse().join("");
+  twoReversedLetters.push(reversedLetters); 
+  alert(twoReversedLetters);
+}
+
+let twoReversedLetters = [];
+
+function seperateAndReverse() {
+  firstLetterSeperator();
+  reverseLetters();
+  const newSentence = sentence + twoReversedLetters;
+  letterJoin.push(newSentence);
+}
+
+let letterJoin = [];
+
+function middleLetter() {
+ sentenceLength = sentence.length;
+ middleLetterIndex = Math.floor(sentenceLength / 2);
+ midLetter = sentence.charAt(middleLetterIndex);
+ const letterJoin2 = midLetter + letterJoin;
+ scrambledSentence.push(letterJoin2.split("").reverse().join(""));
+ alert(scrambledSentence);
+}
+scrambledSentence = [];
+
+
+
+seperateAndReverse();
+middleLetter();
